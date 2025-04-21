a0da91cdca59316f65c4cf79f9c68c9ccb9086dd
MatthewMcDade13 <Matthew.McDade13@tuta.io>
2025-04-21 02:06:53 -0700

    remove rust

--- zeal-llvm/src/compiler.rs
+++
@@ -1,3 +0,0 @@
-struct Compiler {
-    ctx: inkwell::context::Context,
-}
--- zeal/src/main.rs
+++
@@ -1,9 +0,0 @@
-use zeal_ast::Ast;
-
-const PATH: &str = "../test_scripts/loops.zl";
-
-fn main() -> anyhow::Result<()> {
-    let ast = Ast::from_file(PATH)?;
-    println!("{ast}");
-    Ok(())
-}
