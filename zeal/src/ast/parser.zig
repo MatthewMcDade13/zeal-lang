1decf7f722fe3ee9a5247a2599c912a0fcff6cf2
MatthewMcDade13 <Matthew.McDade13@tuta.io>
2025-06-23 13:45:45 -0700

    redo project

--- zeal/src/ast/parser.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-
-pub fn Parser(file_name: *const []u8) @This() {
-    return struct {};
-}
-
-// pub fn parse(filepath: *const []u8) void {
-
-// }
--- zeal/src/core/root.zig
+++
@@ -1,3 +0,0 @@
-pub fn addCore(a: i32, b: i32) i32 {
-    return a + b;
-}
