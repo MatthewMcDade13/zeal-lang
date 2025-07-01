66f2b8ee12b3d8a95747ce682dc2a9d363a1f8cc
MatthewMcDade13 <Matthew.McDade13@tuta.io>
2025-07-01 12:00:20 -0700

    sigh. back to zig i guess

--- zl-ast/src/lib.rs
+++ zl-ast/src/lib.rs
@@ -1,5 +1,10 @@
+use lib0_rs::zedalloc::gen_alloc::GenAllocator;
+
 pub mod err;
 pub mod expr;
 pub mod lex;
 pub mod parse;
 pub mod walk;
+
+#[global_allocator]
+static GLOBAL: GenAllocator = GenAllocator;
--- zl-mem/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![no_std]
-#![feature(allocator_api)]
-
-pub mod gen_alloc;
-pub mod miheap;
-
-extern crate alloc;
